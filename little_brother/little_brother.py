abde901cdc0c0a41038495cb238d1d9f5954d250
KhalilWang <hsgwpj@gmail.com>
2024-01-23 21:11:55 +0800

    add svr code

--- little_brother/little_brother.py
+++ little_brother/little_brother.py
@@ -1,18 +1,36 @@
 from flask import Flask, request, jsonify
+import os
+import requests
 
 app = Flask(__name__)
 
 # 配置文件路径
-template_file_path = 'prometheus_template.yml'
+generated_config_path = '../deploy/prometheus.yml'
+prometheus_reload_url = 'http://localhost:9090/-/reload'
 
 def generate_prometheus_config(data_list):
     global_config = "global:\n  scrape_interval: 15s\n\nscrape_configs:\n"
-    
+
+    data_list.append({
+        "job_name": "Prometheus",
+        "url": "localhost:9090"
+    })
+
     scrape_configs = ""
     for data in data_list:
-        scrape_configs += f"  - job_name: {data['job_name']}\n    static_configs:\n      - targets:\n        - {data['url']}\n"
+        scrape_configs += f"  - job_name: {data['job_name']}\n    static_configs:\n      - targets:\n        - \"{data['url']}\"\n"
+    scrape_configs += "\n"
 
     return f"{global_config}{scrape_configs}"
+
+def reload_prometheus_config():
+    try:
+        response = requests.post(prometheus_reload_url)
+        response.raise_for_status()
+        return True
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to reload Prometheus config: {e}")
+        return False
 
 @app.route('/generate_config', methods=['POST'])
 def generate_config():
@@ -27,11 +45,15 @@ def generate_config():
 
         prometheus_config = generate_prometheus_config(data_list)
 
-        # 将生成的 Prometheus 配置写入文件（或进行其他处理）
-        with open('prometheus_generated_config.yml', 'w') as prom_file:
+        # 将生成的 Prometheus 配置写入文件到指定目录
+        with open(generated_config_path, 'w') as prom_file:
             prom_file.write(prometheus_config)
 
-        return jsonify({'message': 'Prometheus config generated successfully'}), 200
+        # 重新加载 Prometheus 配置
+        if reload_prometheus_config():
+            return jsonify({'message': 'Prometheus config generated and reloaded successfully'}), 200
+        else:
+            return jsonify({'error': 'Failed to reload Prometheus config'}), 500
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
